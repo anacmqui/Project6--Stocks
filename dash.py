e95d6e7051f91aa51346577e901103d55bdb8dcb
anacmqui <anacmquintino@gmail.com>
2023-01-31 14:51:40 +0100

    Create dash.py

---
+++ dash.py
@@ -0,0 +1,7 @@
+import dash                                                     # pip install dash  (2.1 or higher)
+from dash import html, dcc, Input, Output, State, dash_table
+import pandas as pd                                             # pip install pandas
+import plotly.express as px
+import pymongo                                                  # pip install "pymongo[srv]"
+from bson.objectid import ObjectId
+
