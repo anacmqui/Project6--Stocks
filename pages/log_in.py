37c2e38fee294b0e366f13dbfcd9e32dad466f80
anacmqui <anacmquintino@gmail.com>
2023-02-07 21:45:05 +0100

    add

--- pages/log_in.py
+++ pages/log_in.py
@@ -9,4 +9,27 @@ from plotly.subplots import make_subplots
 
 dash.register_page(__name__, path='/login')
 
-layout = html.Div(children=[])
+email_input = html.Div([dbc.Label("Email", html_for="example-email"),
+                        dbc.Input(type="email", id="example-email", placeholder="Enter email"),
+                        ],
+                        className="mb-3",
+                        )
+
+password_input = html.Div([
+                        dbc.Label("Password", html_for="example-password"),
+                        dbc.Input(type="password", id="example-password", placeholder="Enter password")
+                            ],
+                    className="mb-3",
+                )
+
+form = dbc.Form([email_input, password_input])
+button = dbc.Button("Enter your portfolio")
+output_container = html.Div(className="mt-4")
+
+layout = html.Div([
+            dbc.Row(children = [
+                    html.H1(
+                     children = ['Build your portfolio'], style={'textAlign':'center', "padding": "2rem 1rem", 'color':'#8B1A1A'}
+                     ), ]),
+            dbc.Container([form, button, output_container], fluid=True)
+                    ])
